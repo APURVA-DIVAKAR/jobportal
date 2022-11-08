a3f5d34ec2d46cdef1485ddece708a48ad7e6c73
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-11-08 10:01:20 +0530

    vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const QuestionModel = require('./Model/Questions')
 const JobModel = require('./Model/Job')
 const app = express()
 const mongoose = require('mongoose')
@@ -16,12 +17,48 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 
 app.get('/',(req,res)=>res.send(`hello`))
 
-
 app.get("/api/jobs",async(req, res)=>{
     const jobs = await JobModel.find({})
     res.send(jobs);
 })
+// app.post("/api/questions",async(req,res)=>{
+//     const {category,difficultyLevel,question,correctAnswer,option1,option2,option3} = req.body;     
+//      const que = new QuestionModel({category,difficultyLevel,question,correctAnswer,option1,option2,option3})
+//      await que.save();
+//      res.status(201).send({
+//          message:"que Created Succesfully!"
+//      })
 
+// })
+
+// app.get("/api/quiz",async(req, res)=>{
+//     const quiz = await QuestionModel.find({})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/easy",async(req, res)=>{
+//     const quiz = await QuestionModel.find({difficultyLevel:"Easy"})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/medium",async(req, res)=>{
+//     const quiz = await QuestionModel.find({difficultyLevel:"Medium"})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/hard",async(req, res)=>{
+//     const quiz = await QuestionModel.find({difficultyLevel:"Hard"})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/sports",async(req, res)=>{
+//     const quiz = await QuestionModel.find({category:"Sports"})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/geographical",async(req, res)=>{
+//     const quiz = await QuestionModel.find({category:"Geographical"})
+//     res.send(quiz);
+// })
+// app.get("/api/quiz/music",async(req, res)=>{
+//     const quiz = await QuestionModel.find({category:"Music"})
+//     res.send(quiz);
+// })
 
 
 
